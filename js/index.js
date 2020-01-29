f37c11c500cc58dc0a032cb34a09b66812777001
Sandeep KD <9041662611.SK@gmail.com>
2020-01-29 07:02:16 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ x.on(np,function(){n=$(this).val();y=x.index(this);
 	if(y==0&&n.match(/^[0-9]+$/)){dv=n; bv=c(n,10,2); hv=c(n,10,16); av=String.fromCharCode(n);}
 	else if(n!=''){if(y==1&&n.match(/^[0-1]+$/)){dv=c(n,2,10);}
 	if(y==2&&n.match(/^[A-Fa-f0-9]+$/)){dv=c(n,16,10);}
-	if(y==3){dv=n.charCodeAt();} d.val(dv); d.trigger(np);} as(n);
+	if(y==3){dv=n.charCodeAt(n.length-1);} d.val(dv); d.trigger(np);} as(n);
 });
 function c(s,f,t){return parseInt(s, f).toString(t);}
 function as(n){if(n==''){dv='',bv='',hv='',av='';}d.val(dv);b.val(bv);h.val(hv);a.val(av);}
