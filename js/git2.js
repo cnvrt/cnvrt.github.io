e9315dad70c243201d2797ff29340251c0407ab2
Sandeep Kumar <60330723+cnvrt@users.noreply.github.com>
2025-03-10 15:00:41 +0530

    Update git2.js

--- js/git2.js
+++ js/git2.js
@@ -1,4 +1,4 @@
-style = `\n<style>.container{margin-top: 15%;}.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>`;
+style = `\n<style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>`;
 function git(url = '#', position = 'top-right', color = 'white-black') {
   const vpos = {
     'top-left': '-9',
